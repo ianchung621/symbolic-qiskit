f5335c421436a694a7c9fc2c6c6f3a30b2c724e4
Hung-Yen Chang <ianchung621@gmail.com>
2025-06-20 11:57:02 +0800

    [feat] enforce all parameter in pqc to be real

--- symbolic_qiskit/gate/utils.py
+++ symbolic_qiskit/gate/utils.py
@@ -7,9 +7,16 @@ def parse_param(p):
         return float(p)
     elif isinstance(p, qcc.ParameterExpression):
         expr_str = str(p).replace('[', '_').replace(']', '')
-        return parse_expr(expr_str)
+        return parse_real_expr(expr_str)
     else:
         raise TypeError(f"Unsupported parameter type: {type(p)}")
+
+def parse_real_expr(expr_str: str) -> sp.Expr:
+    
+    def real_symbol(name):
+        return sp.Symbol(name, real=True)
+
+    return parse_expr(expr_str, local_dict={}, global_dict={"Symbol": real_symbol})
 
 def sp_exp_i(x):
     return sp.exp(sp.I * x)
