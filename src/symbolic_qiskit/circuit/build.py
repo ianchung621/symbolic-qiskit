dc52761afe81babd6981800516e7a2d53b5c963c
Hung-Yen Chang <ianchung621@gmail.com>
2025-06-24 14:00:24 +0800

    [feat] use decompose instead of transpile

--- src/symbolic_qiskit/circuit/build.py
+++ src/symbolic_qiskit/circuit/build.py
@@ -1,13 +1,63 @@
 from qiskit import QuantumCircuit, transpile
+from qiskit.circuit import Instruction
 
 from .base import QCLayer, Chunk, ChunkedCircuit, StandardGateChunk, MeasurementChunk, StandardGateLayer, BarrierLayer, MeasurementLayer
 from ..layer import circuit_to_layers
 from ..gate import SUPPORTED_GATES
 
 def circuit_to_chunks(qc: QuantumCircuit) -> ChunkedCircuit:
-    transpiled_qc = transpile(qc, basis_gates=SUPPORTED_GATES, optimization_level=1)
-    layers = circuit_to_layers(transpiled_qc)
-    return ChunkedCircuit(layers_to_chunks(layers), transpiled_qc.global_phase)
+    supported_gates = SUPPORTED_GATES | {'delay','measure','barrier'}
+    unsupported_gates = {'reset','global_phase'}
+    decomposed_qc = decompose_to_standard_gates(qc, supported_gates, unsupported_gates)
+    layers = circuit_to_layers(decomposed_qc)
+    return ChunkedCircuit(layers_to_chunks(layers), decomposed_qc.global_phase)
+
+def decompose_to_standard_gates(
+    quantum_circuit: QuantumCircuit,
+    supported_gates: set[str],
+    unsupported_gates: set[str]
+) -> QuantumCircuit:
+    
+    def needs_decompose(op: Instruction):
+        return (
+            op.name not in supported_gates
+            and hasattr(op, 'definition')
+            and op.definition is not None
+        )
+
+    qc = quantum_circuit
+
+    while True:
+        decomposed = False
+        new_qc = QuantumCircuit(*qc.qregs, *qc.cregs)
+
+        for ins in qc.data:
+            op: Instruction = ins.operation
+            qargs, cargs = ins.qubits, ins.clbits
+
+            if op.name in unsupported_gates:
+                raise ValueError(f"Unsupported gate '{op.name}' encountered.")
+
+            if needs_decompose(op):
+                sub_qc: QuantumCircuit | None = op.definition.copy()
+                sub_qc = sub_qc.assign_parameters(op.params)
+                sub_qc = sub_qc.decompose()
+
+                for sub_ins in sub_qc.data:
+                    new_qc.append(
+                        sub_ins.operation,
+                        [qargs[i] for i in range(len(sub_ins.qubits))],
+                        cargs
+                    )
+                decomposed = True
+            else:
+                new_qc.append(op, qargs, cargs)
+
+        qc = new_qc
+        if not decomposed:
+            break
+
+    return qc
 
 def layers_to_chunks(layers: list[QCLayer]) -> list[Chunk | BarrierLayer]:
     result = []
